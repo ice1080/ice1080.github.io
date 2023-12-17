279f2865407d139792ac8c55cce69e9a642586e2
Isaac Hudson <ice1080@gmail.com>
2023-12-17 15:16:20 -0700

    adding data re-retrieval

--- main.js
+++ main.js
@@ -1,52 +1,67 @@
 /* TODO
-   - add quantity specifier
    - refresh button?
  */
 
 const apiKey = '982d766304c44b02abc23ffb1472af56'
 
-const gridQuantity = document.querySelector('#ag-grid-quantity').value;
-// TODO on grid quantity change
+const quantityElement = document.querySelector('#ag-grid-quantity')
+const gridQuantity = quantityElement.value;
 
 function randomDate(start, end) {
-  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
 
 const startDate = new Date(2010, 0, 1);
 const endDate = new Date();
 
-async function generateGrid() {
+const gridOptions = {
+    rowData: [],
+    columnDefs: [
+        { field: "id" },
+        { field: "name" },
+        /* { field: "managerId" }, */
+        { field: "startDate" },
+    ]
+};
 
-    let response = await fetch('https://randommer.io/api/Name?nameType=fullName&quantity=' + gridQuantity, {
-        headers: {
-            'X-Api-Key': apiKey,
-        }
-    });
-    let namesJson = await response.json();
+const myGridElement = document.querySelector('#myGrid');
+const gridApi = agGrid.createGrid(myGridElement, gridOptions);
+
+let namesList = []
+
+async function getNewNames(quantityToRetrieve) {
+    if (quantityToRetrieve > 0) {
+        let response = await fetch('https://randommer.io/api/Name?nameType=fullName&quantity=' + quantityToRetrieve, {
+            headers: {
+                'X-Api-Key': apiKey,
+            }
+        });
+        const namesJson = await response.json();
+
+        namesList = namesList.concat(namesJson);
+    } else {
+        return
+    }
+}
+
+async function generateGrid(gridQuantity) {
+    await getNewNames(gridQuantity - namesList.length);
 
     const rowData = [];
     for (let i = 0; i < gridQuantity; i++) {
         rowData.push({
-            id: i+1,
-            name: namesJson[i],
+            id: i + 1,
+            name: namesList[i],
             /* managerId: '', */
             startDate: randomDate(startDate, endDate),
         });
     }
 
-    const gridOptions = {
-        // Row Data: The data to be displayed.
-        rowData: rowData,
-        columnDefs: [
-            { field: "id" },
-            { field: "name" },
-            /* { field: "managerId" }, */
-            { field: "startDate" },
-        ]
-    };
-
-    const myGridElement = document.querySelector('#myGrid');
-    agGrid.createGrid(myGridElement, gridOptions);
+    gridApi.setGridOption('rowData', rowData)
 }
 
-generateGrid();
+generateGrid(gridQuantity);
+
+quantityElement.addEventListener('change', function (e) {
+    generateGrid(e.target.value);
+});
